e991c555f1fc70b70147a4834dd292ee3e50c44f
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-10-02 19:31:11 +0300

    Add files via upload

---
+++ work3.cpp
@@ -0,0 +1,194 @@
+#include <iostream>
+using namespace std;
+int main() {
+//7
+/*	int a, b;
+	cin >> a >> b;
+	if (a < b) {
+		cout << 1 << endl;
+	}
+	else if (b < a) {
+		cout << 2 << endl;
+	}
+	else if (a == b) {
+		cout << "=" << endl;
+	}*/
+//8
+/*	int a, b;
+	cin >> a >> b;
+	if (a < b) {
+		cout << b << endl;
+		cout << a << endl;
+	}
+	else if {
+		cout << a << endl;
+		cout << b << endl;
+	}
+	else if (a == b) {
+		cout << "=" << endl;
+	}*/
+//9
+/*	int a, b, c;
+	cin >> a >> b;
+	if (a > b) {
+		double t = a;
+		a = b;
+		b = t;
+		cout << a << endl;
+		cout << b << endl;
+	}
+	cout << a << " " << b << endl;*/
+//10
+/*	int a, b;
+	cin >> a >> b;
+	if (a != b) {
+		itn sum = a + b;
+		b = sum
+	}
+	else {
+		a = b = 0;
+	}*/
+//11
+/*	int a, b;
+	cin >> a >> b;
+	if (a != b) {
+		if (a > b) {
+			b = a;
+		}
+		else if (b > a) {
+			a = b;
+		}
+	}
+	else {
+		a = 0;
+		b = 0;
+	}
+	cout << a << " " << b << endl;*/
+//12
+/*	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b && b > c || b > a && a > c) {
+		cout << c << endl;
+	}
+	else if (b > c && c > a || c > b && b > a) {
+		cout << a << endl;
+	}
+	else if (c > a && a > b || a > c && c > b) {
+		cout << b << endl;
+	}*/
+//13
+/*	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b && b > c) {
+		cout << b << endl;
+	}
+	else if (c > b && b > a) {
+		cout << b << endl;
+	}
+	else if (b>a && a > c) {
+		cout << a << endl;
+	}
+	else if (c > b && b > a) {
+		cout << a << endl;
+	}
+	else if (b > c && c > a){
+		cout << c << endl;
+	}
+	else if (a > c && c > b) {
+		cout << c << endl;
+	}*/
+//14
+/*	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b && b > c) {
+		cout << a << endl;
+		cout << c << endl;
+	}
+	else if (c > b && b > a) {
+		cout << c << endl;
+		cout << a << endl;
+	}
+	else if (b > a && a > c) {
+		cout << b << endl;
+		cout << c << endl;
+	}
+	else if (c > b && b > a) {
+		cout << c << endl;
+		cout << a << endl;
+	}
+	else if (b > c && c > a) {
+		cout << b << endl;
+		cout << a << endl;
+	}
+	else if (a > c && c > b) {
+		cout << a << endl;
+		cout << b << endl;
+	}*/
+//15
+/*	int a, b, c, cym;
+	cin >> a >> b >> c;
+	if (a > b && b > c) {
+		cym = a + b;
+		cout << cym << endl;
+	}
+	else if (c > b && b > a) {
+		cym = c + b;
+		cout << cym << endl;
+	}
+	else if (b > a && a > c) {
+		cym = b + a;
+		cout << cym << endl;
+	}
+	else if (c > a && a > b) {
+		cym = c + a;
+		cout << cym << endl;
+	}
+	else if (b > c && c > a) {
+		cym = b + c;
+		cout << cym << endl;
+	}
+	else if (a > c && c > b) {
+		cym = a + c;
+		cout << cym << endl;
+	}*/
+//16
+/*	int a, b, c;
+	cin >> a >> b >> c;
+	if (c > b && b > a) {
+		a = a * 2;
+		b = b * 2;
+		c = c * 2;
+	}
+	else {
+		a = -a;
+		b = -b;
+		c = -c;
+	}
+	cout << a << " " << b << " " << c << endl;*/
+//17
+/*	int a, b, c;
+	cin >> a >> b >> c;
+	if (c > b && b > a || a > b && b > c) {
+		a = a * 2;
+		b = b * 2;
+		c = c * 2;
+	}
+	else {
+		a = -a;
+		b = -b;
+		c = -c;
+	}
+	cout << a << " " << b << " " << c << endl;*/
+//18
+/*	int a, b, c;
+	cin >> a >> b >> c;
+	if (c == b) {
+		cout << "1" << endl;
+	}
+	else if (a == c) {
+		cout << "2" << endl;
+	}
+	else if (a = b)
+		cout << "3" << endl;*/
+	return 0;
+}
